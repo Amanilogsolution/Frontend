22220230f886be9fc0159fbc4589fc6c5e9212b7
Amanilogsolution <aman@ilogsolution.com>
2022-04-26 10:40:14 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import {Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import PrivatRoute from './component/HOC/PrivateRoute';
 import Login from './component/Login/Login';
 import Home from './component/Home/Home'
@@ -51,9 +51,14 @@ import PageNotFound from './component/pagenotfound/pagenotfound';
 
  const App = () => {
 
+   setTimeout(() => {
+    localStorage.removeItem('Token')
+   },8000)
+
     return (
       <div>
       <Router>
+        <Switch>
           <Route exact path="/" restricted={false} component={Login}/>
           <Route  exact path="/home" component={Home}/>
           <Route exact path="/Customer" component={Customer}/>
@@ -75,7 +80,7 @@ import PageNotFound from './component/pagenotfound/pagenotfound';
           <Route exact path="/Showcity" component={Showcity}/>
           <Route exact path="/Addcity" component={Addcity}/>
           <Route exact path='/EditCity' component={EditCity}/>
-          <Route exact path="/ShowUnit" component={ShowUnit}/>
+          <PrivatRoute exact path="/ShowUnit" component={ShowUnit}/>
           <Route exact path="/AddUnit" component={AddUnit}/>
           <PrivatRoute exact path="/EditUnit" component={EditUnit}/>
 
@@ -99,7 +104,7 @@ import PageNotFound from './component/pagenotfound/pagenotfound';
           <Route exact path="/TotalVendAddress" component={TotalVendAddress}/>
           <Route exact path="/EditVendorAddress" component={EditVendorAddress}/>
           <Route exact path="*" component={PageNotFound}/>
-        
+          </Switch>
         </Router>
         
       </div>
--- src/component/Master/Currency/ShowCurrency.js
+++ src/component/Master/Currency/ShowCurrency.js
@@ -3,11 +3,15 @@ import Header from "../../Header/Header";
 import Menu from "../../Menu/Menu";
 import Footer from "../../Footer/Footer";
 import { currency } from '../../../api';
+import {ImportCurrency} from '../../../api'
 import DataTable from 'react-data-table-component';
 // import Excelfileform '../countires-codes-and-currencies.xlsx'
 import DataTableExtensions from 'react-data-table-component-extensions';
 import 'react-data-table-component-extensions/dist/index.css';
 import {deleteCurrency} from '../../../api';
+import Excelfile from '../../../tbl_countries.xlsx';
+// import Modal from '../Modal/modal'
+import * as XLSX from "xlsx";
 
 const columns = [
     {
@@ -52,26 +56,6 @@ const columns = [
            </div>
         ]
        },
-      //  {
-      //   name:'Active',
-      //   selector: 'null',
-      //   cell: (row) => [
-      //       <input type='checkbox' checked={row.status== 'Active'}  onClick={async(e) =>
-      //         {
-      //           if(row.status == 'Active'){
-      //             const checkvalue ='Deactive'
-      //             await deleteCurrency(row.sno,checkvalue)
-      //                 window.location.href='ShowCurrency'
-    
-      //           }
-      //           else{
-      //             const checkvalue ='Active'
-      //             await deleteCurrency(row.sno,checkvalue)
-      //                 window.location.href='ShowCurrency'
-      //           }
-      //          }} />
-      //   ]
-      // },
     {
         name: "Actions",
         sortable: false,
@@ -87,11 +71,66 @@ const columns = [
 
  const ShowCurrency = () => {
     const [data,setData] = useState([])
+    const [importdata, setImportdata] = useState([]);
+
+    const handleClick = () => {
+      // const Jsondata = JSON.stringify(data)
+      const array = JSON.stringify(importdata)
+      const datas = JSON.parse(array)
+      ImportCurrency(datas)
+      
+      console.log(importdata)
+      // props.function()
+      // window.location.reload()
+    };
+    const onChange = (e) => {
+      const [file] = e.target.files;
+      const reader = new FileReader();
+  
+      reader.onload = (evt) => {
+        const bstr = evt.target.result;
+        const wb = XLSX.read(bstr, { type: "binary" });
+        const wsname = wb.SheetNames[0];
+        const ws = wb.Sheets[wsname]; // console.log(result);
+        const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
+        // console.log(data);
+        var lines = data.split("\n");
+  
+        var result = [];
+  
+        var headers = lines[0].split(",");
+  
+        for (var i = 1; i < lines.length-1; i++) {
+          var obj = {};
+          var currentline = lines[i].split(",");
+  
+          for (var j = 0; j < headers.length; j++) {
+            obj[headers[j]] = currentline[j];
+          }
+          result.push(obj);
+        }
+        setImportdata(result);
+        // console.log(result);
+      };
+      reader.readAsBinaryString(file);
+    };
+
+    // if(importdata.length>0){
+    //   console.log("Hello Data Uploaded")
+    //   console.log(importdata)
+    //   ImportCurrency(importdata)
+    // }
 
      useEffect(async() => {
        const result = await currency()
        setData(result)
      }, [])
+
+    //  const Imported = async() => {
+    //    console.log('Done',importdata)
+    //   const result = await ImportCurrency(importdata)
+    //   console.log(result)
+    //  }
 
      const tableData  ={
         columns,data
@@ -108,11 +147,9 @@ const columns = [
           <div>
             <div className="content-wrapper">
             <button type="button" style={{float:"right",marginRight:'10%',marginTop:'1%'}} onClick={()=>{window.location.href="./AddCurrency"}} className="btn btn-primary">Add Currency</button>
-            {/* <button type="button" style={{float:"right",marginRight:'2%',marginTop:'1%'}} onClick={()=>{window.location.href="#"}} className="btn btn-success"  data-toggle="modal"
-                                data-target="#exampleModal">Import excel file</button> */}
+            <button type="button" style={{float:"right",marginRight:'2%',marginTop:'1%'}} onClick={()=>{window.location.href="#"}} className="btn btn-success"  data-toggle="modal" data-target="#exampleModal">Import excel file</button>
               <div className="container-fluid">
                   <br/>
-                
                 <h3 className="text-left ml-5">Currency</h3>
                 <br/>
                 <div className="row ">
@@ -143,7 +180,8 @@ const columns = [
             </div>
           </div>
           <Footer />
-           {/* ------------------ Modal start -----------------------------*/}
+           {/* ------------------ Modal start -----------------------------*/}\
+           {/* <Modal excel={Excelfile} importdatas={setImportdata} /> */}
            <div
               className="modal fade"
               id="exampleModal"
@@ -156,7 +194,7 @@ const columns = [
                 <div className="modal-content">
                   <div className="modal-header">
                     <h5 className="modal-title" id="exampleModalLabel">
-                      Import data
+                      Import excel file
                     </h5>
                     <button
                       type="button"
@@ -168,42 +206,7 @@ const columns = [
                     </button>
                   </div>
                   <div className="modal-body">
-                    {/* <div className=" ">
-                      <label
-                        htmlFor="user_name"
-                        className=" col-form-label font-weight-normal"
-                      >
-                        <span style={{ color: "red" }}> Currency Code *</span>
-                      </label>
-                      <div className="col form-group ">
-                        <select
-                          id="inputState"
-                          className="form-control col-md-10 "
-                        >
-                          <option selected> AED- UAE Dirham</option>
-                          <option>AUD- Australian Dollar</option>
-                          <option>CAD- Canadian Dollar</option>
-                          <option>CNY- Yuan Renminbi</option>
-                          <option>EUR- Euro</option>
-                          <option>INR- Indian Rupee</option>
-                        </select>
-                      </div>
-                    </div> */}
-                    {/* <div className=" ">
-                      <label
-                        htmlFor="user_name"
-                        className=" col-form-label font-weight-normal"
-                      >
-                        <span style={{ color: "red" }}> Currency Symbol *</span>
-                      </label>
-                      <div className="col form-group ">
-                        <input
-                          id="addsymbol"
-                          type="text"
-                          className="form-control col-md-10"
-                        />
-                      </div>
-                    </div> */}
+                    
                     <div className=" ">
                       <label
                         htmlFor="user_name"
@@ -215,26 +218,24 @@ const columns = [
                         <input
                           id=""
                           type="file"
+                          onChange={onChange} 
                           className="form-control "
                           accept="application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"/>
                       </div><br/>
-                    <span style={{color:"red"}}>Please! select the excel file only and in this formate otherwise your data is stored in wrong formate.
-                       <a href='#' > Excel file</a>
+                    <span style={{color:"red"}}>
+                       <a href={Excelfile} download> Download formate</a>
                     </span><br/>
-
                     </div>
-
-
                   </div>
                   <div className="modal-footer">
-                    <button
+                  <button
                       type="button"
                       className="btn btn-secondary"
                       data-dismiss="modal"
                     >
                       Close
                     </button>
-                    <button type="button" className="btn btn-primary">
+                    <button type="button" onClick={handleClick} className="btn btn-primary"  data-dismiss="modal">
                       Upload
                     </button>
                   </div>
